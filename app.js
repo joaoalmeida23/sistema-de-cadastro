b24bc2b261806c66d2996dbf66ead456567090a6
João Almeida <122281054+joaoalmeida23@users.noreply.github.com>
2023-02-03 20:07:41 +0000

    corrigindo app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 
 console.log("jogo instalado")
+console.log("Enviando para o  GitHub")
